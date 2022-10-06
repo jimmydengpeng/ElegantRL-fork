37262747cfc7791e32d8a37ea24f2bd5dd03880e
JimmyDeng <121dengpeng@163.com>
2022-10-07 04:18:25 +0800

    small fix

--- elegantrl/train/config.py
+++ elegantrl/train/config.py
@@ -282,4 +282,5 @@ def set_attr_for_env(env, env_args):
 
 
 '''for type checkinh'''
-DEFAULT_ARGS = Arguments(agent_class=None)  # type: ignore
+# DEFAULT_ARGS = Arguments(agent_class=None) 
+DEFAULT_ARGS: Arguments = None # type: ignore
